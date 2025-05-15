a2dcc4ba4f542950f8593ec4c6cae4287c6bd7a2
Alejo <67307555+AlejoVides@users.noreply.github.com>
2025-05-15 20:56:21 -0300

    ej5

--- lab01/ej5/fixstring.c
+++ lab01/ej5/fixstring.c
@@ -1,24 +1,31 @@
 #include <stdbool.h>
 #include <assert.h>
-
 #include "fixstring.h"
 
-
 unsigned int fstring_length(fixstring s) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    int length = 0;
+    while (s[length] != '\0') length++;
+    return length;
 }
 
 bool fstring_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool res = true;
+    unsigned int i = 0;
+    while (s1[i] != '\0' && s1[i] == s2[i]) i++;
+    return res = s1[i] == s2[i];
 }
 
 bool fstring_less_eq(fixstring s1, fixstring s2) {
-    /* copiá acá la implementación que hiciste en el ejercicio 0 */
+    bool res = true;
+    unsigned int i = 0;
+    while (s1[i] != '\0' && s1[i] == s2[i]) 
+        i++;
+    return (s1[i] <= s2[i]);
 }
 
 void fstring_set(fixstring s1, const fixstring s2) {
-    int i=0;
-    while (i<FIXSTRING_MAX && s2[i]!='\0') {
+    int i = 0;
+    while (i < FIXSTRING_MAX && s2[i] != '\0') {
         s1[i] = s2[i];
         i++;
     }
@@ -27,10 +34,7 @@ void fstring_set(fixstring s1, const fixstring s2) {
 
 void fstring_swap(fixstring s1,  fixstring s2) {
     fixstring aux;
-    /*
-     * COMPLETAR
-     *
-     */
+    fstring_set(aux, s1);
+    fstring_set(s1, s2);
+    fstring_set(s2, aux);
 }
-
-
--- lab01/ej5/sort.c
+++ lab01/ej5/sort.c
@@ -10,15 +10,33 @@
 
 
 static unsigned int partition(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    unsigned int pivot, i, j;
+    pivot = izq;
+    i = izq+1;
+    j = der;
+
+    while(i <= j){
+        if(goes_before(a[i], a[pivot])) {
+            i++;
+        } else if(goes_before(a[pivot], a[j])) {
+            j--;
+        } else if(!goes_before(a[i], a[pivot]) && a[j] < a[pivot]) {
+            swap(a, i, j);
+        }
+    }
+    swap(a, pivot, j);
+    pivot = j;
+    return pivot;
 }
 
 static void quick_sort_rec(fixstring a[], unsigned int izq, unsigned int der) {
-    /* copiá acá la implementación que hiciste en el ejercicio 3 */
+    if(der > izq) {
+        unsigned int pivot = partition(a, izq, der);
+        quick_sort_rec(a, izq, (pivot == 0) ? 0 : pivot-1);
+        quick_sort_rec(a, (pivot+1), der);
+    }
 }
 
 void quick_sort(fixstring a[], unsigned int length) {
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
-
-
