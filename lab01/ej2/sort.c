da50b09e13a89d6f99fb51bb513d8d840508bed9
Alejo <67307555+AlejoVides@users.noreply.github.com>
2025-05-15 20:54:38 -0300

    ej2

--- lab01/ej2/sort.c
+++ lab01/ej2/sort.c
@@ -6,17 +6,14 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
-
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
-    printf("se llamo con izq=%u der=%u\n", izq, der);
-    unsigned int pivot;
     if(der > izq) {
-        pivot = partition(a, izq, der);
+        unsigned int pivot = partition(a, izq, der);
         quick_sort_rec(a, izq, (pivot == 0) ? 0 : pivot-1);
         quick_sort_rec(a, (pivot+1), der);
     }
 }
+
 void quick_sort(int a[], unsigned int length) {
     quick_sort_rec(a, 0, (length == 0) ? 0 : length - 1);
 }
-
